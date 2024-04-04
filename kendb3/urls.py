945f74cda26d3da8b2d4ad3fd8f6bf0ab3c9758d
OLEGSHA <kvadropups@gmail.com>
2024-04-04 02:38:22 +0200

    Hotfix: '/' now redirects to '/submissions/'

--- kendb3/urls.py
+++ kendb3/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path('', include('profiles.urls')),
     path('', include('submissions.urls')),
+    path('', RedirectView.as_view(url='/submissions/')),
     path('api/v0/get/', include('api_lib.urls')),
     path('admin/', admin.site.urls),
 ]

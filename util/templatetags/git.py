c0dcef168f6e7c32a9328f0d7f35d3a61c4d6ad2
OLEGSHA <kvadropups@gmail.com>
2024-04-07 23:01:52 +0200

    Git SHA is now truncated

--- util/templatetags/git.py
+++ util/templatetags/git.py
@@ -1,19 +1,17 @@
 import os
+from functools import cache
 from django import template
 
 register = template.Library()
 
 
-def return_env(name, env_var, default=''):
-    """Create a function that returns the value of an environment variable.
-
-    Queried environment variables are assumed to be constant.
-    """
-    value = os.getenv(env_var, default=default)
-    result = lambda: value
-    result.__name__ = name
-    return result
+@register.simple_tag
+@cache
+def git_ref():
+    return os.getenv('GIT_REF', '(dev)')
 
 
-register.simple_tag(return_env('git_ref', 'GIT_REF', '(dev)'))
-register.simple_tag(return_env('git_sha', 'GIT_SHA', '-'))
+@register.simple_tag
+@cache
+def git_sha():
+    return os.getenv('GIT_SHA', '-')[:9]
